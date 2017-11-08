0b58e44edff9c26a19df8ed842e6e46b05acdb84
forsvarir <forsvarir@blueyonder.co.uk>
2017-11-08 15:52:14 +0000

    Completed wiring up to use prism to invoke main window

--- StudyAid/StudyAid.Gui/App.xaml.cs
+++ StudyAid/StudyAid.Gui/App.xaml.cs
@@ -13,5 +13,11 @@ namespace StudyAid.Gui
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ApplicationBootstrapper bootstrapper = new ApplicationBootstrapper();
+            bootstrapper.Run();
+        }
     }
 }
---
+++ StudyAid/StudyAid.Gui/ApplicationBootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+using StudyAid.Gui.Views;
+
+namespace StudyAid.Gui
+{
+
+    public class ApplicationBootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+        }
+    }
+}
